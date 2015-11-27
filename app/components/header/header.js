260f8fa3b0855910c85e46235bb53fd69484e2ac
Gabriel Mayta <grandemayta@gmail.com>
2015-11-28 00:08:24 +0100

    developing homepage

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -19,6 +19,7 @@ let Header = React.createClass({
                         <div className="nav-wrapper">
                             <a href="#" className="brand-logo">Logo</a>
                             <a href="#" className="button-collapse">
+
                                 <i className="material-icons">menu</i>
                             </a>
                             <ul className="right hide-on-med-and-down">
