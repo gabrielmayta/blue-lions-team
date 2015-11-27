dc008766d000abd95f22c3cb7a37a51f9dde1125
Gabriel Mayta <grandemayta@gmail.com>
2015-11-28 00:08:01 +0100

    developing homepage

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -20,17 +20,13 @@ let Header = React.createClass({
                             <a href="#" className="brand-logo">Logo</a>
                             <a href="#" className="button-collapse">
                                 <i className="material-icons">menu</i>
-
                             </a>
                             <ul className="right hide-on-med-and-down">
-
                                 <li>
                                     <a href="#">Rosa 2015/2016</a>
-
                                 </li>
                                 <li>
                                     <a href="#">Punto Sangatti</a>
-
                                 </li>
                                 <li>
                                     <a href="#">Arruolati</a>
@@ -39,12 +35,9 @@ let Header = React.createClass({
                             <ul className="side-nav">
                                 <li>
                                     <a href="#">Rosa 2015/2016</a>
-
                                 </li>
                                 <li>
                                     <a href="#">Punto Sangatti</a>
-
-
                                 </li>
                                 <li>
                                     <a href="#">Arruolati</a>
